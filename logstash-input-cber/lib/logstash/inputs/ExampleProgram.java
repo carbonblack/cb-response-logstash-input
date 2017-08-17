61952976b8c582a1751d97e9acf381a956f5e32a
Zachary Estep <zestep@carbonblack.com>
2017-08-17 18:40:45 -0400

    Hackathon: CbR Logstash input plugin!

---
+++ logstash-input-cber/lib/logstash/inputs/ExampleProgram.java
@@ -0,0 +1,15 @@
+
+import com.carbonblack.SensorEvents.SensorEventsProtos.*;
+
+
+class ExampleProgram {
+      public static void main(String[] args){
+             try {
+                CbEventMsg.Builder cbmsgBuilder = CbEventMsg.newBuilder();
+             }
+             catch (Exception e) {
+                e.printStackTrace(System.out);
+             }
+              System.out.println("I'm a Simple Program");
+                }
+}
